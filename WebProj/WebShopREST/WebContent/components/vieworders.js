d611bad0de28f291f50a5947fd9e9c4049dcbf2a
JovanKatanic <jovan.katanic0101@gmail.com>
2023-06-27 00:19:15 +0200

    iznajmljivanje: prikaz tabele o iznajmljivanjima za kupca(prvi dio)

--- WebProj/WebShopREST/WebContent/components/vieworders.js
+++ WebProj/WebShopREST/WebContent/components/vieworders.js
@@ -7,8 +7,29 @@ Vue.component("vieworders", {
 	},
 	template: ` 
 		<div>
+			<table>
+		      <tr>
+		        <th>Order Id</th>
+		        <th>Agency Name</th>
+		        <th>Start Date Time</th>
+		        <th>Duration(in days)</th>
+		        <th>Price</th>
+		        <th>Firstname</th>
+		        <th>Lastname</th>
+		        <th>Status</th>
+		      </tr>
+		      <tr v-for="o in orders">
+		        <td>{{o.order_id}}</td>
+		        <td>{{o.agency.name}}</td>
+		        <td>{{o.date_time.dayOfMonth +'-'+ o.date_time.month+'-'+o.date_time.year+' '+o.date_time.hour+':'+o.date_time.minute}}</td>
+		        <td>{{o.duration}}</td>
+		        <td>{{o.price}}</td>
+		        <td>{{o.firstname}}</td>
+		        <td>{{o.lastname}}</td>
+		        <td>{{o.status}}</td>
+      		  </tr>
+    		</table>
 			
-
 
 		</div>
     
