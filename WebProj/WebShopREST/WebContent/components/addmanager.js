2e2abe147fd3556a633c3b6805c7169ca0bd6f1e
JovanKatanic <jovan.katanic0101@gmail.com>
2023-06-29 22:15:41 +0200

    Administrator_dodavanje_menadzera: fali agency id

--- WebProj/WebShopREST/WebContent/components/addmanager.js
+++ WebProj/WebShopREST/WebContent/components/addmanager.js
@@ -1,8 +1,19 @@
  Vue.component("addmanager", {
 	data: function () {
 		    return {
-		      	username:null
-		      	
+		      	username:null,
+		      	user:
+				  	{
+			        username: "",
+			        password: "",
+			        first_name: "",
+			        last_name: "",
+			        gender: null,
+			        birth_date: null,
+			        type: "Manager",
+			        points:0,
+			        rank:"BRONZE"//treba agency id
+			  		}
 		    }
 	},
 	template: `
@@ -10,24 +21,24 @@
 		<table>
 	        <tr>
 	            <td>Username:</td>
-	            <td><input type="text"></td>
+	            <td><input type="text" v-model="user.username"></td>
 	        </tr>
 	        <tr>
 	            <td>Password:</td>
-	            <td><input type="password"></td>
+	            <td><input type="password" v-model="user.password"></td>
 	        </tr>
 	        <tr>
 	            <td>First_name:</td>
-	            <td><input type="text"></td>
+	            <td><input type="text" v-model="user.first_name"></td>
 	        </tr>
 	        <tr>
 	            <td>Last_name:</td>
-	            <td><input type="text"></td>
+	            <td><input type="text" v-model="user.last_name"></td>
 	        </tr>
 	        <tr>
 	            <td>Gender:</td>
 	            <td>
-	                <select>
+	                <select v-model="user.gender">
 	                    <option value="Male">Male</option>
 	                    <option value="Female">Female</option>
 	                </select>
@@ -35,17 +46,24 @@
 	        </tr>
 	        <tr>
 	            <td>Birth_date</td>
-	            <td><input type="date"></td>
+	            <td><input type="date" v-model="user.birth_date"></td>
 	        </tr>
 	        <tr>
 	            <td>Rental_agency_id:</td>
 	            <td><input type="number"></td>
 	        </tr>
+	        <tr>
+	            <td></td>
+	            <td><input type="button" @click="addManager" value="Add"></td>
+        	</tr>
     	</table>
     </div>	
     `
 	, 
 	methods : {
+		addManager:function(){
+			axios.post('rest/users/',this.user);
+		}
 		
 	},
 	mounted () {	
